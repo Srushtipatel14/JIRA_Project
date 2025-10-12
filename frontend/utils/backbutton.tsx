642e28384356a5db8c218ce4a7f88784e094b7f7
Srushtipatel14 <srushtip579@gmail.com>
2025-10-12 23:18:04 +0530

    change done

--- frontend/utils/backbutton.tsx
+++ frontend/utils/backbutton.tsx
@@ -1,15 +1,10 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import { FiArrowLeft } from "react-icons/fi";
 
 const BackButton = () => {
     const router = useRouter();
     return (
-        <button
-            className="admin_city_add"
-            style={{ cursor: "pointer" }}
-            onClick={() => router.back()}
-        >
+        <button className="admin_city_add" style={{ cursor: "pointer" }} onClick={() => router.back()}>
             Back
         </button>
     )
